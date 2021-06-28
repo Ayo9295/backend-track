c8d3dc77ec46d4fa31df078ab894696cad187f04
Ayo9295 <85228415+Ayo9295@users.noreply.github.com>
2021-06-28 20:55:32 +0100

    Add files via upload

--- backend-weekly-task-main/src/week-4/httpRequest.js
+++ backend-weekly-task-main/src/week-4/httpRequest.js
@@ -1,10 +1,32 @@
-const fetch = require("node-fetch");
-async function getUsers() {
-        let res = await fetch('https://jsonplaceholder.typicode.com/users');
-        let data = await res.json();
-        return data.map((user) =>{
-            console.log(user.name)
-            return user.name;
-        })
+
+
+const https = require('https')
+
+let request = https.get ('https://jsonplaceholder.typicode.com/users', (res) => {
+    if (res.statusCode !== 200) {
+        console.error( `did not get an ok from the server. code: ${res.statusCode}`);
+        res.resume();
+        return;
     }
-    getUsers()
+    let data ='';
+    res.on('data', (chunk) => {
+        data += chunk;
+    })
+    res.on('close', () => {
+        data = JSON.parse(data);
+        console.log('retrieved all data');
+        console.log(JSON.parse(data));
+
+        for(let i = 0; i < data.length; i++) {
+            console.log(data[i].name);
+        }
+    })
+
+    
+
+});
+
+request.on('error', (err)=> {
+    console.error(`encountered an error trying to make arequest: ${err.message}`);
+});
+
